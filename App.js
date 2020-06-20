85c88c206991cea1f8e0138914eb249c2e084aaa
Bryan <blindaa121@gmail.com>
2020-06-19 21:34:08 -0700

    Create header component

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import Header from './components/header'
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello World</Text>
-      <Image source={{uri: 'https://randomuser.me/api/portraits/men/1.jpg'}} style={styles.img}/>
+      <Header />
+      {/* <Image source={{uri: 'https://randomuser.me/api/portraits/men/1.jpg'}} style={styles.img}/> */}
     </View>
   )
 };
@@ -13,18 +14,25 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  text: {
-    color: 'darkslateblue',
-    fontSize: 35
-  },
-  img: {
-    width: 100,
-    height: 100,
-    borderRadius: 100 / 2
+    paddingTop: 60
   }
-});
+})
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center'
+//   },
+//   text: {
+//     color: 'darkslateblue',
+//     fontSize: 35
+//   },
+//   img: {
+//     width: 100,
+//     height: 100,
+//     borderRadius: 100 / 2
+//   }
+// });
 
 export default App;
